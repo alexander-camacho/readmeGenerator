d0ef673517d88079454bd207af76ddba79809a3e
Alex Camacho <alsbrain@optonline.net>
2020-12-04 10:34:33 -0800

    added generated readme shell

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const inquirer = require('inquirer')
 inquirer.prompt([
     {
         type: 'input',
-        message: 'What is your name?',
-        name: 'name',
+        message: 'What is your project title?',
+        name: 'title',
     },
     {
         type: 'input',
-        message: 'What is your location?',
-        name: 'location',
+        message: 'Describe the project.',
+        name: 'description',
     },
     {
         type: 'input',
@@ -29,27 +29,41 @@ inquirer.prompt([
 
 
     // Declare the file name
-    const filename = 'index.html'
+    const filename = 'readmeGenerator.md'
 
     // Write to the file using the writeFile function, takes in the file name and what is being entered within the file.
-    fs.writeFile(filename, 
+    fs.writeFile(filename,
+
+    `# ${response.title}
+
+
+## Description
+${response.description}
+
+## Table of Contents
+    *Lorem
         
-        `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-            <title>Document</title>
-        </head>
-        <body>
-            <h1 id='name' class='jumbotron text-center'>Hi my name is ${response.name}</h1>
-            <p id='location' class='text-center'>${response.location}</p>
-            <p class='text-center'><a id='linkedin' target='_blank'  href='${response.linkedin}'>${response.linkedin}</a></p>
-            <br>
-            <p class='text-center'><a id='github' target='_blank'  href='${response.github}'>${response.github}</a></p>
-        </body>
-        </html>`        
+    *Ipsum
+           
+    *Dolor
+           
+## Installation
+${response.installation}
+
+## Usage
+${response.usage}
+
+## License
+${response.license}
+
+## Contributing
+${response.contributing}
+
+## Tests
+${response.tests}
+           
+## Questions
+${response.questions}`
         ,
-    (err) => err ? console.log(err) : console.log('done'))
+        (err) => err ? console.log(err) : console.log('done'))
 })
